25371c6fecde61fa7bdd16bb9ddfe93190a1b6f5
Carlos Ibáñez <68730621+carlosiba2811@users.noreply.github.com>
2021-03-04 18:33:43 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,16 @@
 
+
+// Download the file and put the file in the data folder
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
+
+// Read the data from the files into the variables
+// Read the activity files
